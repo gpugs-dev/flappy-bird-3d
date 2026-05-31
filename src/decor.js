a9904b7e1fb4dd62d093f70f8d5425c04d2c5cf3
gpugs-dev <lexaost@bk.ru>
2026-05-31 23:58:45 +0300

    Phase 5: add star decor

--- src/decor.js
+++ src/decor.js
@@ -5,17 +5,17 @@ export class Decor {
     this.game = game;
     this.scene = game.sceneSetup.scene;
     this.items = [];
-    this.speed = -6;
+    this.speed = -4;
     this.spawnTimer = 0;
 
     const data = game.assets.models['decor.glb'];
     this.template = data.scene.clone(true);
-    this.template.scale.setScalar(15);
+    this.template.scale.setScalar(2);
   }
 
   update(delta) {
     this.spawnTimer += delta;
-    if (this.spawnTimer > 2 + Math.random() * 3) {
+    if (this.spawnTimer > 1 + Math.random() * 2) {
       this.spawnTimer = 0;
       this.spawn();
     }
@@ -23,7 +23,7 @@ export class Decor {
     for (let i = this.items.length - 1; i >= 0; i--) {
       const item = this.items[i];
       item.position.x += this.speed * delta;
-      if (item.position.x < -25) {
+      if (item.position.x < -30) {
         this.scene.remove(item);
         this.items.splice(i, 1);
       }
@@ -33,9 +33,9 @@ export class Decor {
   spawn() {
     const clone = this.template.clone(true);
     clone.position.set(
-      25,
-      1 + Math.random() * 4,
-      (Math.random() - 0.5) * 16
+      30,
+      2 + Math.random() * 8,
+      -5 - Math.random() * 8
     );
     this.scene.add(clone);
     this.items.push(clone);
